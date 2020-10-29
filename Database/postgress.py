5b528473957df29f371d8a21def4f4bacda069ee
Aditya Patil <Aditya.patil0404@gmail.com>
2020-10-29 20:45:43 +0530

    Initial  Commit
    Select operation

---
+++ Database/postgress.py
@@ -0,0 +1,16 @@
+#%%
+import psycopg2 as pg
+
+#%%
+conn = pg.connect(host="localhost", port = 5432, database="helloworld", user="postgres", password="root")
+
+cur = conn.cursor()
+
+cur.execute("""SELECT * FROM world""")
+query_results = cur.fetchall()
+print(query_results)
+
+cur.close()
+conn.close()
+
+#//TODO CU
