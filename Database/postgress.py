34eb04ae6f2118469499552421a805ae8baeea8f
Aditya Patil <Aditya.patil0404@gmail.com>
2020-11-04 21:17:03 +0530

    Connected to  database.ini

--- Database/postgress.py
+++ Database/postgress.py
@@ -2,12 +2,7 @@
 import psycopg2 as pg
 
 #%%
-conn = pg.connect(
-    host="localhost", 
-    port = 5432, 
-    database="helloworld", 
-    user="postgres", 
-    password="root")
+
 
 cur = conn.cursor()
 
@@ -18,6 +13,24 @@ print(query_results)
 cur.close()
 conn.close()
 
+#%%
+from configparser import ConfigParser
 
+def config(filename='database,ini', section='postgresql'):
+    parser =ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1}'.format(section, filename))
+
+        return db
 #//TODO Insert
 #//TODO Update, Delete
+
+# %%
