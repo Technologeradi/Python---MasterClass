853de890741794efaeb1a44e9ed940bd589b025c
Aditya Patil <Aditya.patil0404@gmail.com>
2020-11-09 22:40:15 +0530

    Bug Fixs

--- Database/Connect.py
+++ Database/Connect.py
@@ -1,13 +1,21 @@
-import psycopg2 as pg
+import psycopg2
 from Config import config
-def connect():
-    conn = None
-    try:
-        params = config()
-        print('Connecting to the posgress')
-        conn = pg.connect(**params)
-        cur = conn.cursor()
 
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = psycopg2.connect("dbname=helloworld user=postgres password=root")
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect("dbname=helloworld user=postgres password=root2")
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
 
@@ -17,15 +25,9 @@ def connect():
        
 	# close the communication with the PostgreSQL
         cur.close()
-    except (Exception, pg.DatabaseError) as error:
+    except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
             print('Database connection closed.')
-
-
-if __name__ == '__main__':
-    connect()
-         
-#//TODO Debug the Connection
